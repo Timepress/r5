01e6ba6f097dae20e1b6ada521d52274afe877c7
Michal <michalhorcic@gmail.com>
2016-11-02 18:18:38 +0100

    Add instructions how to use wicked_pdf after installation

--- lib/r5/recipes/wicked_pdf.rb
+++ lib/r5/recipes/wicked_pdf.rb
@@ -7,7 +7,32 @@ add_gem 'wkhtmltopdf-binary'
 run 'bundle install'
 run 'rails generate wicked_pdf'
 
-say "CHECK https://github.com/mileszs/wicked_pdf", :green
+instructions = <<-TXT
+INSTRUCTIONS:
+Quick test: add 'render pdf: "file_name"' to your controller action
+(show action is usually the easiest bet) )
+
+More advanced example:
+  footer_html = render_to_string('offers/footer.html.erb', layout: 'layouts/pdf.html')
+  header_html = render_to_string('offers/header.html.erb', layout: 'layouts/pdf.html')
+  body_html = render_to_string('offers/print.html.erb', layout: 'layouts/pdf.html')
+
+  pdf = WickedPdf.new.pdf_from_string(
+          body_html,
+          header: {content: header_html, spacing: 5},
+          footer: {content: footer_html},
+          page_size: 'A4',
+          margin: {top: 25, bottom: 45, left: 10, right: 10},
+
+  )
+  send_data pdf, filename: "offer_number.pdf",
+            type: 'application/pdf',
+            disposition: 'inline'
+
+FOR MORE INFORMATION: https://github.com/mileszs/wicked_pdf
+TXT
+
+say instructions, :green
 say "For server - find binary with which wkhtmltopdf and used it as exe_path in config/initializers/wicked_pdf.rb", :red
 
 
--- lib/r5/version.rb
+++ lib/r5/version.rb
@@ -1,3 +1,3 @@
 module R5
-  VERSION = "0.2.5.1"
+  VERSION = "0.2.5.2"
 end
